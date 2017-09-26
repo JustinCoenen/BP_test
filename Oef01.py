d9e4ca200fcab89c8d98201f7273d7aa7d422c12
JustinCoenen <Justin.coenen@student.howest.be>
2017-09-26 09:32:25 +0200

    Initial commit

---
+++ Oef01.py
@@ -0,0 +1,33 @@
+# Dit is commentaar (commentarieer je programma)
+# Labo_intro oef1
+
+
+#naam= input("Wat is jouw naam?")
+#voornaam = "Justin"
+#naam = "Coenen"
+#leeftijd= int(input("hoe jong ben jij?"))
+#leeftijd = leeftijd + 10
+#print("Welkom", voornaam, "\t",naam)
+
+#print( "Welkom {0}. Jouw naam is {1}. \nJij bent {2:.2f} jaar.".format(voornaam, naam, leeftijd))
+
+#basis = float(input("Wat is de basis van de driehoek?"))
+#hoogte = float(input("Wat is de hoogte van de driehoek?"))
+#opp = 1.68
+
+#print("De basis is {1: .2f}. De hoogte is {2: .2f}. Dus de opp is 1.68".format(opp, basis, hoogte))
+
+
+
+
+
+dagen = int(input("Geef het aantal dagen op: ")) * 60*60 * 24
+uren = int(input("geef het aantal uren op: ")) * 3600
+minuten = int(input("Geef het aantal minuten op: ")) * 60
+seconden = int(input("Geef het aantal seconden op: "))
+seconds = dagen + uren + minuten + seconden
+
+print("Het aantal seconden is {0}.".format(seconds))
+
+#print("De aantal dagen zijn {0}. De aantal uren zijn {1}. Het aantal minuten zijn {2}. Het aantal seconden is {3}.".format(dagen, uren, minuten, seconden))
+
